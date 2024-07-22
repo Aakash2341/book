42a5ccc8468d5ed125ae5c7add32f6264297e63c
Aakash2341 <aakashverma2341@gmail.com>
2024-07-23 01:39:45 +0530

    Your commit message

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import User from "../model/user.model.js";
 
 export const signup = async (req, res) => {
@@ -15,7 +15,7 @@ export const signup = async (req, res) => {
     const newUser = new User({
       fullname,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await newUser.save();
@@ -37,21 +37,25 @@ export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!user || !isMatch) {
-      return res.status(400).json({ message: "invaild username or password" });
-    } else {
-      res.status(200).json({
-        message: "login successful",
-        user: {
-          _id: user._id,
-          fullname: user.fullname,
-          email: user.email,
-        },
-      });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid username or password" });
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        _id: user._id,
+        fullname: user.fullname,
+        email: user.email,
+      },
+    });
   } catch (error) {
-    console.log("error: " + error.message);
-    res.status(500).json({ message: "internal server error" });
+    console.log("Error:", error.message);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
